424bd91c05402f3ca3f15ad805cf7c91d4e18d90
martinri007 <martinri007@gmail.com>
2022-04-28 12:17:01 +0200

    Import Helmet, page title

--- dice-roller/src/App.js
+++ dice-roller/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import RollDice from './RollDice'
+import { Helmet } from "react-helmet";
 function App() {
 
   return (
 
     <div className="App">
+      <Helmet>
+        <title>Hoď si!</title>
+      </Helmet>
       <header className="App-header">
         <div>
           <RollDice />
