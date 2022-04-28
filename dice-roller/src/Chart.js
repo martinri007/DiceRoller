d742556fdef6c6eb8df41a135604c712732bf383
martinri007 <martinri007@gmail.com>
2022-04-28 11:57:14 +0200

    Style, fix

--- dice-roller/src/Chart.js
+++ dice-roller/src/Chart.js
@@ -8,36 +8,38 @@ class MyChart extends Component {
     super(props);
     console.log("RollData jsou " + this.props.rollData);
 
-    
+
     this.state = {
       options: {
         chart: {
           id: "basic-bar"
         },
         xaxis: {
-          categories: [2,3,4,5,6,7,8,9,10,11,12]
+          categories: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        },
+        theme: {
+          mode: 'dark',
+          palette: 'palette10' // upto palette10
         }
+
       },
     };
   }
 
   render() {
-
     return (
-      <div className="row">
-        <div className="mixed-chart">
-          <Chart
-            options={this.state.options}
-            series={[
-              {
-                name: "series-1",
-                data: this.props.rollData,
-              }
-            ]}
-            type="bar"
-            width="500"
-          />
-        </div>
+      <div className="StatsChart" >
+        <Chart
+          options={this.state.options}
+          series={[
+            {
+              name: "Sum occurrence",
+              data: this.props.rollData,
+            }
+          ]}
+          type="bar"
+          width="500"
+        />
       </div>
     );
   }
